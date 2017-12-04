94e55979716ea539e7c7808de3dd4ed5693544f7
Joe DeBruycker <jd.debr@gmail.com>
2017-12-03 22:08:59 -0700

    Hook up review service to mongo, boilerplate for REST API

--- reviews_api/reviews.js
+++ reviews_api/reviews.js
@@ -1,17 +1,35 @@
 'use strict';
 
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
 // Constants
 const PORT = 8080;
 const HOST = '0.0.0.0';
 
+// Database Connections
+const mongoDB = mongoose.connect('mongodb://mongo:27017');
+
 // App
 const app = express();
+const router = express.Router();
 
-app.get('/', (req, res) => {
-	res.send('Hello world\n');
+// Middleware
+router.use((res, req, next) => {
+	console.log("middleware logging");
+	next();
 });
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+router.get('/', (req, res) => {
+	res.json({"message":'Hello reviews'});
+});
+
+app.use('/api', router);
 
 app.listen(PORT, HOST);
 console.log(`Review service running on http://${HOST}:${PORT}`);
