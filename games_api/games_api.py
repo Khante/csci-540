35158c4ec05248098585c603d7833e8e4c32e512
Joe DeBruycker <jd.debr@gmail.com>
2017-12-13 15:19:54 -0700

    Code optimizations

--- games_api/games_api.py
+++ games_api/games_api.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, abort, json
+from flask import Flask, jsonify, abort, json
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -6,44 +6,42 @@ app = Flask(__name__)
 client = MongoClient(host='mongo')
 db = client.gamesdb
 
-@app.route('/games/<string:game_name>', methods = ['GET'])
-def get_game(game_name):
-	game = [game for game in db.games.find() if game['title'].replace(" ","") == game_name]
-	if len(game)==0:
-		abort(404)
-	return jsonify({'description':game[0]['description'], 'platforms':game[0]['platforms'],'score':game[0]['moby_score'], 'title':game[0]['title'], '_id': str(game[0]['_id'])}) 
 
-@app.route('/genre', methods = ['GET'])
-def get_genre():
-	genreList = []
-	for genre in db.genres.find():
-		name1 =  {'description':genre['genre_description'], 'category':genre['genre_category'], 'title':genre['genre_name'], 'genre_id': genre['genre_id']}
-		genreList.append(name1)
-	jsonStr =  json.dumps(genreList)
-	return jsonify(Genres=jsonStr)
-		
-@app.route('/consoles', methods = ['GET'])
-def get_console():
-	consoleList = []
-	for console in db.platforms.find():
-		name1 =  {'platform_id':console['platform_id'], 'title':console['platform_name']}
-		consoleList.append(name1)
-	jsonStr =  json.dumps(consoleList)
-	return jsonify(Consoles=jsonStr)
+@app.route('/games/<string:title>', methods=['GET'])
+def games_by_title(title):
+    query = {"title": title}
+    projection = {"description": 1,
+                  "game_id": 1,
+                  "genres": 1,
+                  "platforms": 1,
+                  "moby_score": 1,
+                  "title": 1,
+                  "_id": 0}
+    return jsonify([game for game in db.games.find(query, projection)])
 
-@app.route('/genre/<string:genre_name>', methods = ['GET'])
+
+@app.route('/genres', methods=['GET'])
+def get_genres():
+    return jsonify([genre for genre in db.genres.find(projection={"_id":0})])
+
+
+@app.route('/consoles', methods=['GET'])
+def get_consoles():
+    return jsonify([console for console in db.platforms.find(projection={"_id":0})])
+
+
+@app.route('/genres/<string:genre_name>', methods=['GET'])
 def get_specific_genre(genre_name):
-	genre = [genre for genre in db.genres.find() if genre['genre_name'].replace(" ","") == genre_name]
-	if len(genre)==0:
-		abort(404)
-	return jsonify({'description':genre[0]['genre_description'], 'category':genre[0]['genre_category'], 'title':genre[0]['genre_name'], 'genre_id': genre[0]['genre_id']}) 
+    query = {"genre_name": genre_name}
+    projection = {"_id": 0}
+    return jsonify([genre for genre in db.genres.find(query, projection)])
 
-@app.route('/consoles/<string:console_name>', methods = ['GET'])
+
+@app.route('/consoles/<string:console_name>', methods=['GET'])
 def get_specific_console(console_name):
-	console = [console for console in db.platforms.find() if console['platform_name'].replace(" ","") == console_name]
-	if len(console)==0:
-		abort(404)
-	return jsonify({'platform_id':console[0]['platform_id'], 'title':console[0]['platform_name']}) 
+    query = {"platform_name": console_name}
+    projection = {"_id": 0}
+    return jsonify([platform for platform in db.platforms.find(query, projection)])
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
